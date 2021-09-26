cd1bd5a07e9d03b43ac0c7f90fea93fb3150295a
sujjang <pnk7038@gmail.com>
2021-09-27 03:03:13 +0900

    3-1 commit(Join.java) - sy
    join.java에서 비밀번호 입력칸과 비밀번호 확인 입력칸의 값이 같지 않으면 회원가입 불가능하도록 변경. (pw_check 함수)
    cnt 변수를 통하여 아이디 중복 확인 절차를 진행하지 않으면 회원가입이 불가능하도록 변경.
    회원가입 성공시 메시지 출력하도록 변경.
    * 처음 중복 확인 버튼 누를때 무조건 중복이라고 뜨는 오류 수정해야 함.

--- app/src/main/java/com/example/proto/Join.java
+++ app/src/main/java/com/example/proto/Join.java
@@ -17,6 +17,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import android.util.Log;
 
 public class Join extends AppCompatActivity{
 
@@ -25,8 +26,9 @@ public class Join extends AppCompatActivity{
     private DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference();
 
     Button id_check, end;
-    EditText ID, PW, NickName, Phone;
+    EditText ID, PW, NickName, Phone,PW_check;
 
+    int cnt=0;
     boolean check;
 
     @Override
@@ -40,14 +42,21 @@ public class Join extends AppCompatActivity{
         PW = (EditText)findViewById(R.id.PW);
         NickName = (EditText)findViewById(R.id.NickName);
         Phone=(EditText)findViewById(R.id.Phone);
+        PW_check=(EditText)findViewById(R.id.PW_check);
 
 
 
         //스피너
-        Spinner spinner = findViewById(R.id.spinner);
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.arr, android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter); //어댑터에 연결
+
+
+        String style=spinner.getSelectedItem().toString();
+
+
+
 
 
         //아이디 중복 확인
@@ -68,19 +77,49 @@ public class Join extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 if (!nullCheck(ID) || !nullCheck(PW) || !nullCheck(NickName)) {
-                    Toast.makeText(getApplicationContext(), "빈칸을 확인해 주세요", Toast.LENGTH_SHORT).show();
-                } else if(id_duplicate_check()==false){
-                    Toast.makeText(getApplicationContext(),"이미 존재하는 ID입니다",Toast.LENGTH_SHORT).show();
-                }else{
-                    signUp(); // signup success
-                    Intent intent = new Intent(Join.this, Login.class); //현재, 이동할 화면
-                    startActivity(intent);
+                    Toast.makeText(getApplicationContext(), "빈칸을 확인해 주세요.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(!pw_check()){ // pw 불일치
+                        Toast.makeText(getApplicationContext(),"비밀번호를 확인해 주세요.",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+
+                        if(cnt==0){
+                            Toast.makeText(getApplicationContext(),"아이디 중복을 확인해 주세요.",Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+
+                            if(id_duplicate_check()==false){
+                                Toast.makeText(getApplicationContext(),"이미 존재하는 ID입니다.",Toast.LENGTH_SHORT).show();
+                            }
+
+                            else {
+                                signUp(style); // signup success
+                                Toast.makeText(getApplicationContext(), "회원가입 성공", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(Join.this, Login.class); //현재, 이동할 화면
+                                startActivity(intent);
+                            }
+                        }
+                    }
+
+
                 }
             }
         });
 
 
 
+    } //oncreate end
+
+
+    // pw 확인 함수
+    public boolean pw_check(){
+        String pw=PW.getText().toString();
+        String check_pw=PW_check.getText().toString();
+
+        if(pw.equals(check_pw)) return true;
+        else return false;
     }
 
     public boolean nullCheck(EditText editText){
@@ -89,7 +128,9 @@ public class Join extends AppCompatActivity{
         else return true;
     }
 
+    // id중복확인 함수
     public boolean id_duplicate_check(){
+        cnt=1;
         databaseReference.child("userAccount").child(ID.getText().toString()).child("id").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -109,12 +150,14 @@ public class Join extends AppCompatActivity{
         return check;
     }
 
-    private void signUp() {
+    private void signUp(String style) {
         String id=((EditText)findViewById(R.id.EditText)).getText().toString();
         String password=((EditText)findViewById(R.id.PW)).getText().toString();
         String nickname=((EditText)findViewById(R.id.NickName)).getText().toString();
+        String phone=Phone.getText().toString();
 
-        UserAccount userAccount=new UserAccount(id,password,nickname);
+
+        UserAccount userAccount=new UserAccount(id,password,nickname,phone,style);
         databaseReference.child("userAccount").child(id).setValue(userAccount);
 
     }
